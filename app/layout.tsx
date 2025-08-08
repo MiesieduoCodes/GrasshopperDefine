eead84a703bde419eaf39dcbc20f4d221965ea40
MiesieduoCodes <miesieduoveria@gmail.com>
2025-08-08 13:45:40 +0100

    omoh

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,8 @@ import { GeistMono } from "geist/font/mono"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.dev",
+  title: "Grasshopper Runner",
+  description: "Created by Goofy",
 }
 
 export default function RootLayout({
